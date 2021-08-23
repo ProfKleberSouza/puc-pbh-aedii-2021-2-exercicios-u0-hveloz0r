c514329a60d59e4a7b4625e045624dbb8f0d43e1
hernane-velozo-puc <hernane.rosa@sga.pucminas.br>
2021-08-23 20:26:13 -0300

    Exercicio 05 - Full

--- exe05/main.c
+++ exe05/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+int main(void) {
+
+    /*<----------<DEFINICAO DE VARIAVEIS>---------->*/
+    int notas[4], i=0, soma=0;
+    float media;
+    /*<----------<ENTRADA DE DADOS>---------->*/
+    for(i=0; i < 4; i++){
+        scanf("%d", &notas[i]);
+        soma += notas[i];
+    }
+    /*<----------<OPERACAO>---------->*/
+    media = (float)soma/i;
+
+    /*<----------<RESULTADO>---------->*/
+    if(media >= 6.5){
+        printf("NOTA = %.1f (APROVADO)", media);
+    }
+    else{
+        printf("NOTA = %.1f (REPROVADO)", media);
+    }
 }
