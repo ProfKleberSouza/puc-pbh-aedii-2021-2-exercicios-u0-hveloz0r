9f7362b02c8700c6e7b759133e5ac12c3bd1af4b
hernane-velozo-puc <hernane.rosa@sga.pucminas.br>
2021-08-23 19:36:25 -0300

    Exercio 03 - Full

--- exe03/main.c
+++ exe03/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+int main(void) {
+
+    /*<----------<DEFINICAO DE VARIAVEIS>---------->*/
+    int num;
+
+    /*<----------<ENTRADA DE DADOS>---------->*/
+    scanf("%d", &num);
+
+    /*<----------<RESULTADO>---------->*/
+    if(num % 2 == 0 && num > 0){
+        printf("\nNUMERO PAR POSITIVO");
+    }
+    else if (num % 2 == 1 && num > 0){
+        printf("\nNUMERO IMPAR POSITIVO");
+    }
+    if(num % 2 == 0 && num < 0){
+        printf("\nNUMERO PAR NEGATIVO");
+    }
+    else if (num < 0){
+        printf("\nNUMERO IMPAR NEGATIVO");
+    }
+    if(num == 0){
+        printf("\nNUMERO NEUTRO");
+    }
 }
