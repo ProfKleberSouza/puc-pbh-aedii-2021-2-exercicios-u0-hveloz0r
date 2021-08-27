2e5006f5b24af9c24b3bef3b55a1a2006e575f21
hernane-velozo-puc <hernane.rosa@sga.pucminas.br>
2021-08-27 11:07:17 -0300

    Exercicio 06 - Full

--- exe06/main.c
+++ exe06/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+int main(void) {
+
+    /*<----------<DEFINICAO DE VARIAVEIS>---------->*/
+    int l1, l2, l3;
+    bool valida = 1;
+    /*<----------<ENTRADA DE DADOS>---------->*/
+    scanf("%d", &l1);
+    scanf("%d", &l2);
+    scanf("%d", &l3);
+    
+    /*<----------<RESULTADO>---------->*/
+    if ((l1 == 0 || l2 == 0 || l3 == 0 ) || (l1+l2<l3 || l1+l3<l2 || l2+l3<l1)){
+        printf("OS LADOS NAO FORMAM UM TRIANGULO\n");
+        //valida = 0;
+    }
+    else if (l1 == l2 && l2 == l3){
+        printf("TRIANGULO EQUILATERO");
+    }
+    else if(l1 == l2 || l1 == l3 || l2 == l3){
+        printf("TRIANGULO ISOCELES");
+    }
+    else
+   {
+      printf("TRIANGULO ESCALENO");
+   }
 }
