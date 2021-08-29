7bbb7c8d654fe6eb9df31d4151cf41176b712af0
hernane-velozo-puc <hernane.rosa@sga.pucminas.br>
2021-08-29 15:53:07 -0300

    Exercicio 10 - Full

--- exe10/main.c
+++ exe10/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+
+int main (void) {
+
+    /*<----------<DEFINICAO DE VARIAVEIS>---------->*/
+    int vetor[10], i=0;
+    
+    /*<----------<ENTRADA DE DADOS>---------->*/
+    for(i = 0; i < 10; i++){
+        scanf("%d", &vetor[i]);
+    }    
+    for(i = 9; i >= 0; i--){
+        printf("%d\n", vetor[i]);
+    }
+
+    /*<----------<RESULTADO>---------->*/
 }
