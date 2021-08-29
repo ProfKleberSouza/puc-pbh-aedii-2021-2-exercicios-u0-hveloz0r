52395dfc05eeb8914fe4ffe2d305662aa3fb8156
hernane-velozo-puc <hernane.rosa@sga.pucminas.br>
2021-08-28 22:25:46 -0300

    Exercicio 04 - Full

--- exe04/main.c
+++ exe04/main.c
@@ -2,23 +2,29 @@
 
 int main() {
 
-int a, b, c;
+  /*<----------<DEFINICAO DE VARIAVEIS>---------->*/
+  int a, b, c;
+  
+  /*<----------<ENTRADA DE DADOS>---------->*/
+  scanf("%d%d%d", &a, &b, &c);
 
-scanf("%d%d%d", &a, &b, &c);
-
-if ((a < b) && (a < c)){
-  printf("MENOR = %d\n", a);
-}if ((b < a) && (b < c)){
-  printf("MENOR = %d\n", b);
-}if ((c < a) && (c < b)){
-  printf("MENOR = %d", c);
-}
-if ((a > b) && (a > c)){
-  printf("MAIOR = %d\n", a);
-}if ((b > a) && (b > c)){
-  printf("MAIOR = %d\n", b);
-}else if ((c > a) && (c > b)){
-  printf("MAIOR = %d", c);
-}
-
+  /*<----------<RESULTADO>---------->*/
+  if ((a < b) && (a < c)){
+    printf("MENOR = %d\n", a);
+  }
+  if ((b < a) && (b < c)){
+    printf("MENOR = %d\n", b);
+  }
+  if ((c < a) && (c < b)){
+    printf("MENOR = %d", c);
+  }
+  if ((a > b) && (a > c)){
+    printf("MAIOR = %d\n", a);
+  }
+  if ((b > a) && (b > c)){
+    printf("MAIOR = %d\n", b);
+  }
+  else if ((c > a) && (c > b)){
+    printf("MAIOR = %d", c);
+  }
 }
