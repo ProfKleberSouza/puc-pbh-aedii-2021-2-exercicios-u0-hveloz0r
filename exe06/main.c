1f35a3d2a5eb2335362961c8094935795b8ae810
hernane-velozo-puc <hernane.rosa@sga.pucminas.br>
2021-08-28 22:33:02 -0300

    Exercicio 06 - Full

--- exe06/main.c
+++ exe06/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 int main(void) {
 
     /*<----------<DEFINICAO DE VARIAVEIS>---------->*/
     int l1, l2, l3;
-    bool valida = 1;
+
     /*<----------<ENTRADA DE DADOS>---------->*/
     scanf("%d", &l1);
     scanf("%d", &l2);
@@ -15,7 +14,6 @@ int main(void) {
     /*<----------<RESULTADO>---------->*/
     if ((l1 == 0 || l2 == 0 || l3 == 0 ) || (l1+l2<l3 || l1+l3<l2 || l2+l3<l1)){
         printf("OS LADOS NAO FORMAM UM TRIANGULO\n");
-        //valida = 0;
     }
     else if (l1 == l2 && l2 == l3){
         printf("TRIANGULO EQUILATERO");
@@ -24,7 +22,7 @@ int main(void) {
         printf("TRIANGULO ISOCELES");
     }
     else
-   {
-      printf("TRIANGULO ESCALENO");
-   }
+    {
+        printf("TRIANGULO ESCALENO");
+    }
 }
